bb903d317965bda3a52062f45742ecf9b466927b
macaylak <macayla.konig@ucalgary.ca>
2022-10-08 18:01:19 -0600

    fixed spelling errors

---
+++ def myAdd(x, y): ### a function that add.py
@@ -0,0 +1,11 @@
+def myAdd(x, y): ### a function that adds two integers
+    return(x + y)
+    
+a = int(input('Select an integer'))
+print('a = ', a)
+
+b = int(input('Select an integer'))
+print('a\b = ', b)
+
+print('a + b = ', myAdd(a,b))
+
